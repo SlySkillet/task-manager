7901480f2806188fd924ec595c5685710208621d
Simon Conrad <simondbconrad@gmail.com>
2023-04-24 15:26:08 -0400

    create view function for projects list

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+
+
+def projects_view(request):
+    projects = Project.objects.all()
+
+    context = {
+        "projects": projects
+    }
+
+    return render(request, "projects/list.html", context)
