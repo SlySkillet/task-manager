c8d0e67d6cbd877c2cc6782b73eface34ad6f746
Simon Conrad <simondbconrad@gmail.com>
2023-04-24 18:03:52 -0400

    create path for signup form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import login_user, logout_user
+from .views import login_user, logout_user, signup_user
 
 
 urlpatterns = [
+    path("signup/", signup_user, name="signup"),
     path("logout/", logout_user, name="logout"),
     path("login/", login_user, name="login"),
 ]
