d8a8eea63c41f9de8270d83f53379ef75ce137a0
Simon Conrad <simondbconrad@gmail.com>
2023-04-25 14:40:32 -0400

    create url path for Project Form

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import projects_view, tasks_view
+from .views import projects_view, tasks_view, create_project
 
 
 urlpatterns = [
+    path("create/", create_project, name="create_project"),
     path("<int:id>/", tasks_view, name="show_project"),
     path("", projects_view, name="list_projects")
 ]
