fa0c7068edf189c4d807a16bb16577810c7f906f
Simon Conrad <simondbconrad@gmail.com>
2023-04-25 15:15:44 -0400

    create view function for TaskForm

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import TaskForm
 
 
 # Create your views here.
+
+
+def create_task(request):
+    if request == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save()
+            return redirect("list_projects")
+    else:
+        form = TaskForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "tasks/create.html", context)
