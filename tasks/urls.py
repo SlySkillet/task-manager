c03e45f562c37595e8e89921fcc354f6d6937665
Simon Conrad <simondbconrad@gmail.com>
2023-04-25 16:38:37 -0400

    create url path for list view

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_task
+from .views import create_task, view_tasks
 
 
 urlpatterns = [
-    path("create/", create_task, name="create_task")
+    path("mine/", view_tasks, name="show_my_tasks"),
+    path("create/", create_task, name="create_task"),
 ]
