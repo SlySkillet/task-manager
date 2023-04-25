7cdefe57689f3a2029233fe4ad294331cd9e4df8
Simon Conrad <simondbconrad@gmail.com>
2023-04-25 11:32:30 -0400

    create path for tasks view

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import projects_view
+from .views import projects_view, tasks_view
 
 
 urlpatterns = [
+    path("<int:id>/", tasks_view, name="show_project"),
     path("", projects_view, name="list_projects")
 ]
