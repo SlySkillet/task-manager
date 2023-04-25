38124f7ecec0a6c174047d4afe449e4813c9dd69
Simon Conrad <simondbconrad@gmail.com>
2023-04-25 15:58:58 -0400

    fix bug - view saves to db and redirects to home

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import TaskForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
-
+@login_required
 def create_task(request):
-    if request == "POST":
+    if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
             task = form.save()
