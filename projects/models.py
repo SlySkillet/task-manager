7b3523c8dd6b5cccf10e0c08bd475dc7c85b9e00
Simon Conrad <simondbconrad@gmail.com>
2023-05-01 15:24:04 -0400

    create chart model

--- projects/models.py
+++ projects/models.py
@@ -16,3 +16,20 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+# --GANTT CHART EXPERIMENT--
+class Chart(models.Model):
+    name = models.CharField(max_length=200)
+    start_date = models.DateField()
+    responsible = models.ForeignKey(User, on_delete=models.CASCADE)
+    week_number = models.CharField(max_length=2, blank=True)
+    finish_date = models.DateField()
+
+    def __str__(self):
+        return str(self.name)
+
+    def save(self, *args, **kwargs):
+        print(self.start_date.isocalendar()[1])
+        if self.week_number == "":
+            self.week_number = self.start_date.isocalendar()[1]
+        super().save(*args, **kwargs)
