0508a28a6d39f652a0becb112406c62ac2e72f55
Simon Conrad <simondbconrad@gmail.com>
2023-05-01 15:30:39 -0400

    add initial chart experiment to view function to check rendering

--- projects/views.py
+++ projects/views.py
@@ -1,16 +1,46 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Project
+from .models import Project, Chart
 from django.contrib.auth.decorators import login_required
 from .forms import ProjectForm
+import pandas as pd
+import plotly.express as px
+from plotly.offline import plot
 
 # Create your views here.
 
 
+# @login_required
+# def projects_view(request):
+#     projects = Project.objects.filter(owner=request.user)
+
+#     context = {"projects": projects}
+
+#     return render(request, "projects/list.html", context)
+
+# --GANTT CHART EXPERIMENT--
 @login_required
 def projects_view(request):
     projects = Project.objects.filter(owner=request.user)
+    qs = Chart.objects.all()
+    projects_data = [
+        {
+            'Project': x.name,
+            'Start': x.start_date,
+            'Finish': x.finish_date,
+            'Responsible': x.responsible.username,
+        } for x in qs
+    ]
+    df = pd.DataFrame(projects_data)
+    fig = px.timeline(
+        df, x_start="Start", x_end="Finish", y="Project", color="Responsible"
+    )
+    fig.update_yaxes(autorange="reversed")
+    gantt_plot = plot(fig, output_type="div")
 
-    context = {"projects": projects}
+    context = {
+        "projects": projects,
+        "plot_div": gantt_plot,
+        }
 
     return render(request, "projects/list.html", context)
 
