0d2c5d2a815ecc603c869fa4076669496d276a02
Simon Conrad <simondbconrad@gmail.com>
2023-05-01 16:27:55 -0400

    comment out previous work - changing scope of experiment

--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from .forms import TaskForm
 from .models import Task
 from django.contrib.auth.decorators import login_required
-
+import pandas as pd
+import plotly.express as px
+from plotly.offline import plot
 
 # Create your views here.
 
@@ -33,3 +35,30 @@ def view_tasks(request):
     }
 
     return render(request, "tasks/list.html", context)
+
+# --GANTT CHART EXPERIMENT--
+@login_required
+def projects_view(request):
+    projects = Project.objects.filter(owner=request.user)
+    qs = Chart.objects.all()
+    projects_data = [
+        {
+            'Project': x.name,
+            'Start': x.start_date,
+            'Finish': x.finish_date,
+            'Responsible': x.responsible.username,
+        } for x in qs
+    ]
+    df = pd.DataFrame(projects_data)
+    fig = px.timeline(
+        df, x_start="Start", x_end="Finish", y="Project", color="Responsible"
+    )
+    fig.update_yaxes(autorange="reversed")
+    gantt_plot = plot(fig, output_type="div")
+
+    context = {
+        "projects": projects,
+        "plot_div": gantt_plot,
+        }
+
+    return render(request, "projects/list.html", context)
