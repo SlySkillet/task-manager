dc1eb36e15fb286a60c7d2cc70e66cdab5b946df
Simon Conrad <simondbconrad@gmail.com>
2023-05-02 10:32:48 -0400

    create url path for edit_task view

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_task, view_tasks, project_task_chart
+from .views import create_task, view_tasks, project_task_chart, edit_task
 
 
 urlpatterns = [
+    path("<int:id>/edit/", edit_task, name="edit_task"),
     path("<int:id>/", project_task_chart, name="view_chart"),
     path("mine/", view_tasks, name="show_my_tasks"),
     path("create/", create_task, name="create_task"),
