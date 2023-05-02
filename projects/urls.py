b3f47941c264958f7f54c748237c9559f249ef58
Simon Conrad <simondbconrad@gmail.com>
2023-05-02 16:35:14 -0400

    create url path for edit_project view

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import projects_view, tasks_view, create_project
+from .views import projects_view, tasks_view, create_project, edit_project
 
 
 urlpatterns = [
+    path("<int:id>/edit/", edit_project, name="edit_project"),
     path("create/", create_project, name="create_project"),
     path("<int:id>/", tasks_view, name="show_project"),
     path("", projects_view, name="list_projects"),
