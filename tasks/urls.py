41471c18a5a043902d67ab96aa61322dac96fdd7
Simon Conrad <simondbconrad@gmail.com>
2023-05-01 21:25:54 -0400

    register url path for chart view

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_task, view_tasks
+from .views import create_task, view_tasks, project_task_chart
 
 
 urlpatterns = [
+    path("<int:id>/", project_task_chart, name="view_chart"),
     path("mine/", view_tasks, name="show_my_tasks"),
     path("create/", create_task, name="create_task"),
 ]
