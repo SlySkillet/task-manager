2c1d068d053926f7c5fb4494bb491a9e1b9db76a
Simon Conrad <simondbconrad@gmail.com>
2023-05-02 16:34:15 -0400

    fix redirect path

--- projects/views.py
+++ projects/views.py
@@ -54,7 +54,7 @@ def edit_project(request, id):
         form = ProjectForm(request.POST, instance=project)
         if form.is_valid():
             form.save()
-            return redirect("home")
+            return redirect("list_projects")
     else:
         form = ProjectForm(instance=project)
 
